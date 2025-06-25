2fae80f99e9dc8b34c3f6b84553c1cca0f2c7be9
vagrant <dsh964964@gmail.com>
2025-06-25 16:28:13 +0000

    chore: add link for law-serial-number

--- src/components/law-serial-number.vue
+++ src/components/law-serial-number.vue
@@ -4,6 +4,11 @@
             text-sm text-gray-500 text-center my-4
         "
     >
-        渝ICP备2021000340号-2
+        <a
+            href="//beian.miit.gov.cn"
+            target="_blank"
+        >
+            渝ICP备2021000340号-2
+        </a>
     </p>
 </template>
